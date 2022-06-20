a9d18545c6ae802fd5ac62d02947b60b928d821f
Hera Siddiqui <sid.hera22@gmail.com>
2022-06-20 19:15:08 +0530

    second commit

--- Adder.java
+++ Adder.java
@@ -1,18 +1,11 @@
-import java.util.*;
-class Adder{  
-static int add(int a, int b){return a+b;}  
-static double add(double a, double b){return a+b;}  
-}  
-class TestOverloading{  
-public static void main(String[] args){  
-System.out.println(Adder.add(11,11));  
-System.out.println(Adder.add(12.3,12.6));  
+class Adder{
+static int add(int a,int b){return a+b;}
+static int add(int a,int b,int c){return a+b+c;}
 }
-public static void main(String Args[])
-	{
-
-
-	}
-
-
-}  
+class TestOverloading1
+{
+public static void main(String args[])
+{
+System.out.println(Adder.add(11,11));
+System.out.println(Adder.add(11,11,11));
+}}
